eae14947c318799492aa72cce0365835a0d020c8
abhijitdotsharma <sharma.abhijit2050@gmail.com>
2022-07-21 12:36:29 +0530

    Merge pull request #2 from abhijitdotsharma/feat/homepage
    Feat/homepage

---
+++ src/pages/rules-page/RulesPage.jsx
@@ -0,0 +1,21 @@
+import './rules-page.css';
+import { NavigationTop } from '../../components';
+
+export default function RulesPage(){
+
+    return(
+        <div>
+            {/* Navigation Top will be removed later */}
+            <NavigationTop />
+            <div className='rules-wrapper'>
+                <h2>Quiz Rules</h2>
+                <section className='rules-section'>
+                    <p>There are 5 questions in total</p>
+                    <p>Each question gets you 10 more points</p>
+                    <button>Start Quiz</button>
+                </section>
+                <h4>Go Back</h4>
+            </div>
+        </div>
+    )
+}
