a5ca700cb9fb5e03f4ef3e90e95f083619674d2f
abhijitdotsharma <sharma.abhijit2050@gmail.com>
2022-07-23 11:22:56 +0530

    restructured home page temporarily

--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -1,57 +1,41 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Hero from "../../assets/hero-removebg.png";
-import {NavigationTop, Footer} from '../../components';
+import { NavigationTop, Footer } from '../../components';
 import "./home-page.css";
 
-const HeroImage = () => {
-    return(
-        <div className='hero-wrapper'>
-            <img 
-            className='hero-img'
-            src={Hero} 
-            alt="hero" 
-            />
-        </div>
-    )
-}
-
-export const LandingText = () => {
-
-
-    return(
-        <div className="landing-text">
-            <p className="txt-lg">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-            </p>
-
-            <p className="txt-md">
-                Test your football knowledge
-            </p>
-            <button
-            className="btn-landing-text"
-            >Lets go</button>
-        </div>
-    )
-}
-
-const LandingFooter = () => {
-    return(
-        <footer className="landing-footer">
-            <p>Github || Instagram || Portfolio</p>
-        </footer>
-    )
-}
 
 export default function HomePage() {
 
-    return(
+    return (
         <div className="homepage">
             <NavigationTop />
-            <LandingText />
-            <main className="flex">
-                <LandingFooter/>
-                <HeroImage />
-            </main>
+            <div className="landing-text">
+                <p className="txt-lg">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                </p>
+
+                <p className="txt-md">
+                    Test your football knowledge
+                </p>
+                <Link 
+                to="/category-page"
+                className="btn-landing-text"
+                onClick={() => console.log("test")}
+                >Lets Gooo</Link>
+            </div>
+
+            {/* <main className="flex"> */}
+                {/* <footer className="landing-footer">
+                    <p>Github || Instagram || Portfolio</p>
+                </footer> */}
+                <div className='hero-wrapper'>
+                    <img
+                        className='hero-img'
+                        src={Hero}
+                        alt="hero"
+                    />
+                </div>
+            {/* </main> */}
         </div>
     )
 }
