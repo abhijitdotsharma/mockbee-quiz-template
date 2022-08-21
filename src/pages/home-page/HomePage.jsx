f1875fd29e3873d4fe3505da62c516ad0320980f
abhijitdotsharma <sharma.abhijit2050@gmail.com>
2022-08-21 16:41:45 +0530

    Homepage refactored

--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Hero from "../../assets/hero-removebg.png";
+import Hero from "../../assets/quiz-hero.png";
 import { NavigationTop, Footer } from '../../components';
 import "./home-page.css";
 
@@ -9,33 +9,37 @@ export default function HomePage() {
     return (
         <div className="homepage">
             <NavigationTop />
-            <div className="landing-text">
-                <p className="txt-lg">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                </p>
 
-                <p className="txt-md">
-                    Test your football knowledge
-                </p>
-                <Link 
-                to="/category-page"
-                className="btn-landing-text"
-                onClick={() => console.log("test")}
-                >Lets Gooo</Link>
-            </div>
-
-            {/* <main className="flex"> */}
-                {/* <footer className="landing-footer">
-                    <p>Github || Instagram || Portfolio</p>
-                </footer> */}
-                <div className='hero-wrapper'>
+            <main className="landing-container">
+                <div className="img-container">
                     <img
-                        className='hero-img'
+                        className="hero-image"
                         src={Hero}
-                        alt="hero"
+                        alt="hero-image"
                     />
                 </div>
-            {/* </main> */}
+                <main className="txt-container">
+                    <p>
+                        ready to know what you don't know?
+                        <Link className='btn btn-primary txt-sm' to="/category-page">LFG 🚀</Link>
+                    </p>
+                    
+                </main>
+            </main>
+
+            <footer className="landing-footer">
+                <h3 className="footer-txt">
+                    made by <a className="footer-link" href="https://abhijit.super.site">Abhijit</a> 
+                </h3>
+                <p className="footer-socials"> 
+                    <a className="footer-link" href="https://github.com/abhijitdotsharma/popcorn">GitHub</a> 
+                    <span className="footer-link-gap">||</span>
+                    <a className="footer-link" href="https://linkedin.com/in/abhijitdotsharma">LinkedIn</a>
+                    <span className="footer-link-gap">||</span>
+                    <a className="footer-link" href="https://abhijit.super.site">Hire me ?</a>
+                </p>
+            </footer>
+
         </div>
     )
 }
