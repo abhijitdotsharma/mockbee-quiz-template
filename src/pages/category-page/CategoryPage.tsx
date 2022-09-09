7f9c3dd7e0a5233043dbda05e29f1a310358e68f
abhijitdotsharma <sharma.abhijit2050@gmail.com>
2022-09-09 21:20:49 +0530

    <CategoryPage/> and <Category/> refactored to TS

--- src/pages/category-page/CategoryPage.tsx
+++ src/pages/category-page/CategoryPage.tsx
@@ -3,17 +3,18 @@ import { useEffect, useState } from "react";
 import { NavigationTop, Category, Footer } from "../../components";
 import "./category-page.css";
 
+import { CategoryType } from "../../types";
+
 export default function CategoryPage() {
 
-    const [categories, setCategories] = useState([
-        //{categoryName: description: id: _id: }
-    ])
+
+    const [categories, setCategories] = useState<CategoryType []>([])
 
     useEffect(() => {
         // getCategoriesOnLoad()
         (async function getCategoriesOnLoad() {
             const response = await axios.get(`/api/categories`)
-            setCategories(prev => [...prev, ...response.data.categories])
+            setCategories((prev: CategoryType[]): CategoryType[] => [...prev, ...response.data.categories])
         })()
     }, [])
 
